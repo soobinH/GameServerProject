9009bcc99a5bcb5191faeb1137fa4dbe2cbc62c6
황수빈 <114851926+soobinH@users.noreply.github.com>
2024-12-12 06:21:00 +0900

    add panel

--- Assets/Scripts/ConnectionManager.cs
+++ Assets/Scripts/ConnectionManager.cs
@@ -4,21 +4,41 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI; // UI 사용을 위해 추가
 
 public class ConnectionManager : MonoBehaviourPunCallbacks
 {
+    [SerializeField]
+    private Button connectButton; // Inspector에서 버튼 할당
+
     void Start()
     {
-        PhotonNetwork.ConnectUsingSettings(); // 포톤 서버 연결
+        // 버튼에 클릭 이벤트 추가
+        if (connectButton != null)
+        {
+            connectButton.onClick.AddListener(ConnectToServer);
+        }
     }
 
-    public override void OnConnected() // 서버 접속성공시 호출
+    // 서버 연결 함수
+    public void ConnectToServer()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            if (connectButton != null)
+            {
+                connectButton.interactable = false; // 연결 시도 중에는 버튼 비활성화
+            }
+        }
+    }
+
+    public override void OnConnected()
     {
         base.OnConnected();
         Debug.Log(System.Reflection.MethodBase.GetCurrentMethod().Name);
-
-        
     }
+
     public override void OnConnectedToMaster()
     {
         base.OnConnectedToMaster();
@@ -30,7 +50,17 @@ public class ConnectionManager : MonoBehaviourPunCallbacks
     {
         base.OnJoinedLobby();
         Debug.Log(System.Reflection.MethodBase.GetCurrentMethod().Name);
-
         PhotonNetwork.LoadLevel("Lobby");
+    }
+
+    // 연결 실패시 버튼 다시 활성화
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        base.OnDisconnected(cause);
+        Debug.Log($"Disconnected: {cause}");
+        if (connectButton != null)
+        {
+            connectButton.interactable = true;
+        }
     }
 }
